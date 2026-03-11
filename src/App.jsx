2287509491f1c171df873f910c7a137d551e30b5
nethmihimashi108@gmail.com <nethmihimashi108@gmail.com>
2026-03-11 20:43:57 +0530

    Add HeroSection

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 //import viteLogo from '/vite.svg'
 import './App.css'
 import Navigation from './components/Navigation'
+import HeroSection from './components/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <div>
       <Navigation />
+      <HeroSection />
     </div>
   )
 }
