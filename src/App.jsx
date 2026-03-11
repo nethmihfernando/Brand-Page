ccd36ece3e971443abad454a67f16a1d1a857cea
nethmihimashi108@gmail.com <nethmihimashi108@gmail.com>
2026-03-11 20:03:04 +0530

    Remove data from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,7 @@ function App() {
 
   return (
     <div>
-      <nav>
-        <div className='logo'>
-          <img src="/images/brand_logo.png" alt="Brand Logo" />
-        </div>
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-
-        <button>login</button>
-      </nav>
+      
     </div>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,21 @@
 const Navigation = () => {
     
     
-    return
+    return (
+        <nav>
+        <div className='logo'>
+          <img src="/images/brand_logo.png" alt="Brand Logo" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+
+        <button>login</button>
+      </nav>
+    )
 };
 
 export default Navigation;
