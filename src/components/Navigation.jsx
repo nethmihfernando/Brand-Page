924232db74f7c3c11d0df69b48cb9d86aa26fcd0
nethmihimashi108@gmail.com <nethmihimashi108@gmail.com>
2026-03-11 21:05:07 +0530

    .hero

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,19 +2,19 @@ const Navigation = () => {
     
     
     return (
-        <nav>
-        <div className='logo'>
-          <img src="/images/brand_logo.png" alt="Brand Logo" />
-        </div>
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
+        <nav className="container">
+            <div className='logo'>
+                <img src="/images/brand_logo.png" alt="Brand Logo" />
+            </div>
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>
 
-        <button>login</button>
-      </nav>
+            <button>login</button>
+        </nav>
     )
 };
 
